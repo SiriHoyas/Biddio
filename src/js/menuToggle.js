bc6cb72fc00e64b5816ac2257bd08c437f3e684e
Siri Høyås <siri@skjervold.me>
2022-11-19 17:19:53 +0100

    adds navbar across devices. Also adds js functionality to open and close menu on small devices

---
+++ src/js/menuToggle.js
@@ -0,0 +1,4 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+  document.querySelector(".navbar-middle").classList.toggle("hidden");
+  document.querySelector(".navbar-right").classList.toggle("hidden");
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,21 @@
 module.exports = {
   content: ["./*.html", "./*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgDark: "#252530",
+        cardsBgDark: "#343443",
+        borderDark: "#363642",
+        inactiveText: "#7D7D7D",
+        offWhite: "#EBEBEB",
+        primaryPurple: "#654DC7",
+        secondaryPurple: "#453293",
+      },
+      fontFamily: {
+        mainFont: ["K2D", "sans-serif"],
+        bodyFont: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
