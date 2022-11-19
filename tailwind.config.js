0d81d100e0077b654604883d002e186cff42b84b
Siri Høyås <siri@skjervold.me>
2022-11-19 20:53:59 +0100

    adds html and tailwind css for the heading on index page. Fully responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,20 @@
 module.exports = {
   content: ["./*.html", "./*.js"],
   theme: {
+    borderWidth: {
+      DEFAULT: ".2px",
+      0: "1px",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+      6: "6px",
+      8: "8px",
+    },
     extend: {
       colors: {
         bgDark: "#252530",
         cardsBgDark: "#343443",
-        borderDark: "#363642",
+        borderDark: "#4F4F60",
         inactiveText: "#7D7D7D",
         offWhite: "#EBEBEB",
         primaryPurple: "#654DC7",
@@ -16,6 +25,9 @@ module.exports = {
       fontFamily: {
         mainFont: ["K2D", "sans-serif"],
         bodyFont: ["Inter", "sans-serif"],
+      },
+      backgroundImage: {
+        indexGraphicDarkMobile: "url('../src/img/indexGraphicDarkMobile.svg')",
       },
     },
   },
