504e4c6f4e7b1ad1b6eba5efe55932099907e346
Siri Høyås <siri@skjervold.me>
2022-11-28 13:28:26 +0100

    adds stying for all listings, and function to display current bid

--- src/js/navSetUserInfo.mjs
+++ src/js/navSetUserInfo.mjs
@@ -1,10 +1,27 @@
 import { getLocalStorage } from "./components/getLocalstorage.mjs";
 
 const { userName, userCredits, userAvatar } = getLocalStorage();
-console.log(userName, userCredits, userAvatar);
 
-document.querySelector(
-  ".user-avatar"
-).innerHTML = `<img src="${userAvatar}" alt="${userName} user avatar image" class="w-8 h-8 rounded-full" />`;
-document.querySelector(".user-name").innerHTML = userName;
-document.querySelector(".user-credits").innerHTML = `${userCredits} Credits`;
+function displayUserInfo() {
+  document.querySelector(".user-name").innerHTML = userName;
+  if (userAvatar !== "null") {
+    document.querySelector(
+      ".user-avatar"
+    ).innerHTML = `<img src="${userAvatar}" alt="${userName} user avatar image" class="w-8 h-8 rounded-full" />`;
+  }
+}
+
+function displayUserCredits() {
+  const userAvatarContainer = document.querySelector(".user-credits");
+
+  if (userCredits === "1") {
+    userAvatarContainer.innerHTML = `${userCredits} Credit`;
+  } else if (userCredits === "0") {
+    userAvatarContainer.innerHTML = `No credits`;
+  } else {
+    userAvatarContainer.innerHTML = `${userCredits} Credits`;
+  }
+}
+
+displayUserInfo();
+displayUserCredits();
