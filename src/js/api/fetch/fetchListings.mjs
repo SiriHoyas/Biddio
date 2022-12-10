e8f58d286e55eb318bf4bb789ea4ff68271bdb6a
Siri Høyås <siri@skjervold.me>
2022-12-10 19:23:40 +0100

    adds documentation for the fetch listings function

--- src/js/api/fetch/fetchListings.mjs
+++ src/js/api/fetch/fetchListings.mjs
@@ -3,10 +3,18 @@ import { getLocalStorage } from "../../components/getLocalstorage.mjs";
 
 /**
  * This function fetches listings from the API with GET HTTP Method.
- * You can add flags based on what response you need, and also set the sort-order.
+ * You can add more flags based on what response you need, and also set the sort-order. These parameters are optional and have set defaults
  * The function returns the response from the API parsed as JSON.
+ *
+ * @param {string} flags Add more flags than the function has by default
+ * @param {string} order Set order. Either "asc" or "desc"
+ *
+ * @example
+ * ```js
+ * const listings = await getListings(`&offset=0&limit=27_active=true`);
+ * ```
  */
-export async function getListings(flags, order = "desc") {
+export async function getListings(flags = "", order = "desc") {
   const { accessToken } = getLocalStorage();
 
   const options = {
