ac80711af23e147d75529499798d2dca20bb2128
Siri Høyås <siri@skjervold.me>
2022-12-13 11:50:41 +0100

    adds auction expired if countdown is less than zero

--- src/js/time/displayCountdown.mjs
+++ src/js/time/displayCountdown.mjs
@@ -1,15 +1,28 @@
 import { countDown } from "./auctionCountdown.mjs";
 
 export async function displayCountdown(endsAt) {
-  const { daysLeft, hoursLeft, minutesLeft, secondsLeft } = countDown(endsAt);
+  const endTime = Date.parse(endsAt);
+
+  const currentTime = new Date().getTime();
+  const diff = endTime - currentTime;
 
   const countdown = document.querySelector(".countdown");
-  countdown.innerHTML = `
-    <div class="flex  font-mainFont text-xl dark:text-offWhite">
-      <p class="mr-2">${daysLeft}d</p>
-      <p class="mr-2">${hoursLeft}h</p>
-      <p class="mr-2">${minutesLeft}m</p>
-      <p class="mr-2">${secondsLeft}s</p>
-    </div>
+
+  if (diff <= 0) {
+    countdown.innerHTML = `
+    <p class="font-mainFont text-xl dark:text-offWhite">Auction Expired</p>
     `;
+    document.querySelector(".ends-in-title").classList.add("hidden");
+  } else {
+    const { daysLeft, hoursLeft, minutesLeft, secondsLeft } = countDown(endsAt);
+
+    countdown.innerHTML = `
+      <div class="flex  font-mainFont text-xl dark:text-offWhite">
+        <p class="mr-2">${daysLeft}d</p>
+        <p class="mr-2">${hoursLeft}h</p>
+        <p class="mr-2">${minutesLeft}m</p>
+        <p class="mr-2">${secondsLeft}s</p>
+      </div>
+      `;
+  }
 }
