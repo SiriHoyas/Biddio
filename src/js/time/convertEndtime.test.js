a78d510f6562852237fcf0083b81bb75955d7fba
Siri Høyås <siri@skjervold.me>
2022-12-15 15:50:10 +0100

    fixes package json

--- src/js/time/convertEndtime.test.js
+++ src/js/time/convertEndtime.test.js
@@ -2,6 +2,6 @@ import { convertEndtime } from "./convertEndtime.js";
 
 test("Convert endtime from ISO to locale date and time string", () => {
   const { date, time } = convertEndtime("2022-12-14T13:30:00.000Z");
-  expect(date).toEqual("12/14/2022");
+  expect(date).toEqual("14/12/2022");
   expect(time).toEqual("14:30:00");
 });
